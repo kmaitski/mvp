1ceab55726be9ed93cecf3ae1b9f076690cea1d3
kmaitski <kevin.maitski@gmail.com>
2018-02-16 16:40:21 -0800

    See last commit message didnt get all files 1st

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+debugger;
+app.use(express.static(__dirname + '/../src/client/public'));
 
-app.use(express.static('../src/client'));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-  res.end('');
-});
+// app.get('/', (req, res) => {
+//   // console.log(path.join(__dirname + '/index.html'))
+//   // res.sendFile('index.html', {root: '../'});
+//   // res.write(index.html);
+//   // app.use('/', express.static(__dirname + '/'));
+//   // res.sendFile(__dirname + '/index.html', {'root': '../'});
+//   res.end('');
+// });
 
 app.post('/add', (req, res) => {
   req.on('data', function(chunk) {
